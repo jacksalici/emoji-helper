087810e54a051c41481be86d4882a342872c9774
Giacomo Salici <giambo2000@gmail.com>
2022-08-04 00:03:47 +0200

    it works

--- dumper.py
+++ dumper.py
@@ -1,17 +1,39 @@
 import requests as req
 import re
+import json
 
 
 res = req.get('https://unicode.org/Public/emoji/15.0/emoji-test.txt')
 data = res.text
+emojiDict = []
+group=''
+subgroup=''
 
 for line in data.splitlines():
-    group=''
-    subgroup=''
+    
     if re.search('# subgroup:', line):
         subgroup=re.split('# subgroup: ', line)[1]
-        print(subgroup)
     elif re.search('# group: ', line):
         group=re.split('# group: ', line)[1]
-        print('GROUP '+group)
+    elif re.search("(.+); (.+) # (.+) (E[0-9]+.[0-9]+) (.+)", line):
+        
+        match = re.match("(.+); (.+) # (.+) (E[0-9]+.[0-9]+) (.+)", line)
+        e = {
+                'emoji': match.group(3),
+                'desc': match.group(5),
+                'group': group,
+                'subgroup': subgroup,
+                'code': match.group(1),
+                'status': match.group(2),
+                'version': match.group(4),
+            }
+        emojiDict.append(e)
+
+try:
+    fp = open("emoji.json", 'w')
+    json.dump(emojiDict,fp)
+except e:
+    print(e)
+        
+        
 
